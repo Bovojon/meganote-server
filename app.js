72566bc8350490e028e5af311b557fbfc7d495ba
bovojon <bovojon@gmail.com>
2016-06-16 14:17:49 -0400

    Send a hardcoded json response

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res){
+  res.json([
+    {
+      title: 'Notez',
+      body_html: 'Wow'
+    },
+    {
+      title: 'Notesa',
+      body_html: 'Wodw'
+    }
+  ]);
+});
+
 app.listen(3030, function(){ // Not 8000
   console.log('Listening on post 3030')
 });
