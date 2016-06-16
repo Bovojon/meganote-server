8145950f82c559a9af5ae43f9c186e5efd10686d
bovojon <bovojon@gmail.com>
2016-06-16 15:18:24 -0400

    Add mongoose and load notes from db

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+require('dotenv').load();
 var express = require('express');
+var db = require('mongoose');
+
+db.connect(process.env.DB_URI);
 var app = express();
 
+//Specify schema by passing 2nd arg
+var Note = db.model('Note', { title:String });
+
+
 app.get('/', function(req, res){
-  res.json([
-    {
-      title: 'Notez',
-      body_html: 'Wow'
-    },
-    {
-      title: 'Notesa',
-      body_html: 'Wodw'
-    }
-  ]);
+  Note
+    .find()
+    .then(function(notes){
+      res.json(notes);
+    });
 });
 
 app.listen(3030, function(){ // Not 8000
+  console.log('DB: '+process.env.DB_URI);
   console.log('Listening on port 3030')
 });
