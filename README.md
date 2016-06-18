9586b55cd57f190a558600e03e5a60a960951a76
bovojon <bovojon@gmail.com>
2016-06-18 13:20:12 -0400

    Create notes from POST

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Meganote Server
 
- Meganote Server is a REST API for note-taking.
+Meganote Server is a REST API for note-taking.
 
- ## Setup
+## Setup
 
- Copy and edit DOTENV configuration
+Copy and edit DOTENV configuration
 
- ```shell
- cp .env.example .env
- ```
+```shell
+cp .env.example .env
+```
 
- Edit the new `.env` file and fill in the variables with appropriate values.
+Edit the new `.env` file and fill in the variables with appropriate values.
 
- ## Starting the Server
+## Starting the Server
 
- * In development, run `npm run dev` to start Supervisor.
- * In production, run `npm start`.
+* In development, run `npm run dev` to start Supervisor.
+* In production, run `npm start`.
--- app.js
+++ app.js
@@ -2,19 +2,39 @@ require('dotenv').load();
 var express = require('express');
 // var db = require('./config/db'); // maybe use config\db is doesn't work
 var Note = require('./models/note');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
+app.use(bodyParser.json());
 // when you receive get request, do the function below:
 app.get('/', function(req, res){
   Note
-    .find()
+    .find() // returns descendant elements of the selected element.
     .then(function(notes){
       res.json(notes);
+    });
+});
+
+app.post('/', function(req, res){
+  var note = new Note({
+    title: req.body.note.title,
+    body_html: req.body.note.body_html
+  });
+
+  note
+    .save() // mongoosejs method to save note to database
+    .then(function(noteData){
+      res.json({
+        message: 'Successfully created note',
+        note: noteData
+      });
     });
 });
 
