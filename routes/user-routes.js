0af10748c689b8f9286f712088464ad94de339f7
bovojon <bovojon@gmail.com>
2016-07-08 10:03:01 -0400

    Create users without passwords

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,19 @@
 var router = require('express').Router();
+var User = require('../models/user');
 
 // CREATE a user
-app.post('/', function(req, res) {
-  res.json({
-    msg: 'HOORAY!'
+router.post('/', function(req, res) {
+  var user = new User({
+    name: req.body.user.name,
+    username: req.body.user.username,
   });
+
+  user.save()
+    .then(
+      userData => {
+        res.json(userData);
+      }
+    );
 });
 
 module.exports = router;
